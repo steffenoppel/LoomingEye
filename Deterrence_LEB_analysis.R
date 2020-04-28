0403b4e66675240b85a55559a9ac6757c9787e70
Steffen <steffen.oppel@gmail.com>
2020-04-28 11:05:08 +0100

    included BACI in analysis
    included section to test BACI with GLMM, and removed observer 'Maris' from data

--- Deterrence_LEB_analysis.R
+++ Deterrence_LEB_analysis.R
@@ -3,6 +3,10 @@
 ### written by steffen.oppel@rspb.org.uk
 ### ##################################################
 
+## originally created 19 April 2020
+## modified on 28 April 2020 to include Yann's comments
+## removed counts from Maris as they seem to be completely opposite to anyone else
+
 ### Load libraries
 library(ggplot2)
 library(data.table)
@@ -33,11 +37,17 @@ counts$GlobalID<-counts$ParentGlobalID
 
 ### FORMAT DATA (remove columns we don't need and format the rest)
 names(surveys)
-surveys<-surveys[,c(2,3,8:11,13:19)] %>%
+surveys<-surveys[,c(2,3,8:19)] %>%
   rename(Date=`Survey Date`, 
          PeriodCode=`Survey Period Code:`,Location=`Location:`,Phase=`Phase:`, Observer=`Observer(s):`,
          weather=`Sun or Precipitation:`,wind_dir=`Wind Direction:`,Temp=`Air Temperature (°C):`,
-         wind_speed=`Wind Speed (km/h):`, cloud=`Cloud Cover:`, sea=`Sea Conditions:`,vis=`Visibility:`)
+         wind_speed=`Wind Speed (km/h):`, cloud=`Cloud Cover:`, sea=`Sea Conditions:`,vis=`Visibility:`,
+         OthObs=`Other Observer(s):`) %>%
+  mutate(OthObs=ifelse(is.na(OthObs),Observer,OthObs)) %>%
+  mutate(Observer=ifelse(OthObs=="Maris","Maris",Observer)) %>%
+  select (-OthObs)
+
+dim(surveys)
 
 counts<-counts[,c(1:96)] %>% select(-c(1,3,5,which(grepl("SUM",names(counts))),which(grepl("Additional",names(counts))))) %>%
   rename(Time=`Count Time:`, CountSeq=`Count Number:`) %>%
@@ -47,6 +57,7 @@ counts<-counts[,c(1:96)] %>% select(-c(1,3,5,which(grepl("SUM",names(counts))),w
   mutate(Start=dmy_hm(paste(Date,Time,sep=" ")))
 head(counts)
 dim(counts)
+dim(counts %>% filter(Observer != "Maris"))
 summary(counts$Number)
 table(counts$Observer)
 
@@ -63,10 +74,12 @@ ggplot() + geom_histogram(aes(x=N)) + facet_wrap(Phase~Treatment)
 
 
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
-#####     PLOT SIMPLE COMPARISONS WITH ERROR BARS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
+#####     PLOT SIMPLE RAW DATA COMPARISONS WITH ERROR BARS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
 
 SUMMARY<-counts %>% filter(!is.na(Number)) %>% #filter(Number<25) %>%
+  filter(Observer != "Maris") %>%
+  filter(Species=="Long-tailed Duck") %>%
   group_by(Phase,Treatment,GlobalID) %>%
   summarise(N=sum(Number)) %>%
   ungroup() %>%
@@ -74,11 +87,12 @@ SUMMARY<-counts %>% filter(!is.na(Number)) %>% #filter(Number<25) %>%
   summarise(mean=mean(N), lcl=quantile(N,0.025),ucl=quantile(N,0.975))  
 
 
-SUMMARY %>% filter(Phase!="PrePhase1") %>%
-ggplot(aes(y=mean, x=Treatment, colour=Phase)) + geom_point(size=2)+
+SUMMARY %>% #filter(Phase!="PrePhase1") %>%
+
+ggplot(aes(y=mean, x=Treatment)) + geom_point(size=2)+
   geom_errorbar(aes(ymin=lcl, ymax=ucl), width=.03)+
-  guides(colour=guide_legend(title="Phase"))+
-  facet_wrap(~Phase) +
+  #guides(colour=guide_legend(title="Phase"))+
+  facet_wrap(~Phase, ncol=2) +
   xlab("") +
   ylab("Number of birds") +
   theme(panel.background=element_rect(fill="white", colour="black"), 
@@ -92,7 +106,8 @@ ggplot(aes(y=mean, x=Treatment, colour=Phase)) + geom_point(size=2)+
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         panel.border = element_blank())
-#ggsave("LEB_raw_data_all_species.jpg", width=8, height=8)
+#ggsave("LEB_raw_data_LTDU_withMaris.jpg", width=8, height=11)
+ggsave("LEB_raw_data_LTDU_withoutMaris.jpg", width=8, height=11)
 
 
 
@@ -172,7 +187,12 @@ counts %>% filter(!is.na(Number)) %>%
 ### SELECT ONLY THE RECORDS RELEVANT TO THE TEST
 data<- counts %>% filter(!is.na(Number)) %>% 
   filter(Species=="Long-tailed Duck") %>%
-  filter(Phase %in% c("Phase1","Phase2")) %>%
+  #filter(Phase %in% c("Phase1","Phase2")) %>%
+  #filter(Observer != "Maris") %>%
+  mutate(Observer=ifelse(Observer=="Ainar,Mati","Mati,Ainar",Observer)) %>%
+  mutate(Observer=ifelse(Observer=="Ainar,Andrus,Rein,Veljo,Mati,Andres,OtherObservers","OtherObservers",Observer)) %>%
+  mutate(Observer=ifelse(Observer=="Andrus","Andres",Observer)) %>%
+  mutate(Observer=ifelse(Observer=="Veljo,OtherObservers","Veljo",Observer)) %>%
   group_by(Phase,Treatment,CountSeq,PeriodCode,Observer,weather,wind_dir,Temp,wind_speed,cloud,sea,vis,Start) %>%
   summarise(N=sum(Number)) %>%
   mutate(day=yday(Start)) %>%
@@ -192,7 +212,8 @@ head(data)
 RF<- randomForest(N~Phase+Treatment+day+hour+weather+wind_dir+Temp+wind_speed+cloud+sea+vis+Observer, data=data, mtry=5,ntree=500, importance=T)
 RF
 
-
+baselineRF<- randomForest(N~Treatment+day+hour+weather+wind_dir+Temp+wind_speed+cloud+sea+vis+Observer, data=data[data$Phase=="PrePhase1",], mtry=5,ntree=500, importance=T)
+baselineRF
 
 
 
@@ -229,7 +250,7 @@ ggplot(data, aes(x=N, y=pred)) +
 
 ## the default basic plot
 varImpPlot(RF)
-
+varImpPlot(baselineRF)
 
 ## advanced plot for publication
 VAR<-importance(RF, type=1)
@@ -270,7 +291,7 @@ table(data$Observer)
 
 plotdat<-bind_rows(data,data) %>%
   mutate(Treatment=rep(unique(data$Treatment), each=dim(data)[1])) %>%
-  filter(Observer=="Ainar,Mati") ## curtail to only one observer to plot effect irrespective of observer variation
+  filter(Observer=="Mati,Ainar") ## curtail to only one observer to plot effect irrespective of observer variation
 
 plotdat %>%
   mutate(pred.num=predict(RF, newdat=plotdat)) %>%
@@ -297,3 +318,107 @@ plotdat %>%
 
 #ggsave("LEB_Treatment_effect_OtherObservers.jpg", width=8, height=8)
 ggsave("LEB_Treatment_effect_AinarMati.jpg", width=8, height=8)
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+##### 5. CREATE PARTIAL DEPENDENCE PLOT FOR TREATMENT AND PHASE ######
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+## this is the predicted effect of the different treatments accounting for all other variables
+#partialPlot(RF, data, x.var=Treatment) ## does not work for character variables
+
+plotdat<-bind_rows(replicate(10,data, simplify=F)) %>%
+  mutate(Treatment=rep(rep(unique(data$Treatment), each=dim(data)[1]),5)) %>%
+  mutate(Phase=rep(rep(unique(data$Phase), each=dim(data)[1]), each=2)) %>%
+  filter(Observer=="Mati,Ainar") ## curtail to only one observer to plot effect irrespective of observer variation
+
+#plotdat %>% group_by(Phase,Treatment) %>% summarise(N=length(Observer)) ## just to check whether all was done correctly
+
+plotdat %>%
+  mutate(pred.num=predict(RF, newdat=plotdat)) %>%
+  group_by(Phase,Treatment) %>%
+  #summarise(mean=mean(pred.num), lcl=quantile(pred.num,0.25),ucl=quantile(pred.num,0.75)) %>%
+  summarise(mean=mean(pred.num), lcl=mean(pred.num)-0.5*sd(pred.num),ucl=mean(pred.num)+0.5*sd(pred.num)) %>%
+  
+  ggplot(aes(y=mean, x=Treatment)) + geom_point(size=2, colour="firebrick")+
+  geom_errorbar(aes(ymin=lcl, ymax=ucl), width=.03)+
+  scale_y_continuous(limits=c(0,8)) +
+  facet_wrap(~Phase, ncol=2) +
+  xlab("") +
+  ylab("Predicted number of LTDU") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+ggsave("LEB_Treatment_effect_byPhase.jpg", width=8, height=11)
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+##### 5. CONDUCT PARAMETRIC BACI ANALYSIS ######
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+library(glmmTMB)
+
+### MANIPULATE DATA TO INCLUDE BEFORE-AFTER
+head(data)
+
+BACIdata <- data %>% mutate(Before=ifelse(Phase=="PrePhase1","before","after")) %>%
+  mutate(PhaseNum=ifelse(Phase %in% c("Phase2","PostPhase2"),2,1))
+  
+
+## fit models
+m1<-glmmTMB(N~Before+Treatment+Before*Treatment+day+hour+Observer+(1|PhaseNum), data=BACIdata, ziformula=~1,family=nbinom2)
+m0<-glmmTMB(N~Before+Treatment+day+hour+Observer+(1|PhaseNum), data=BACIdata, ziformula=~1,family=nbinom2)
+
+
+## assess significance of interaction effect
+anova(m0,m1)
+m1sum<-summary(m1)
+
+
+
+### PLOT predicted OUTPUT ###
+
+plotdat<-bind_rows(replicate(10,data, simplify=F)) %>%
+  mutate(Treatment=rep(rep(unique(data$Treatment), each=dim(data)[1]),5)) %>%
+  mutate(Phase=rep(rep(unique(data$Phase), each=dim(data)[1]), each=2)) %>%
+  mutate(Before=ifelse(Phase=="PrePhase1","before","after")) %>%
+  mutate(PhaseNum=ifelse(Phase %in% c("Phase2","PostPhase2"),2,1)) #%>%
+  #filter(Observer=="Mati,Ainar")
+
+plotdat %>%
+  mutate(pred.num=predict(m1, newdat=plotdat)) %>%
+  group_by(Phase,Treatment) %>%
+  summarise(mean=mean(pred.num), lcl=mean(pred.num)-0.5*sd(pred.num),ucl=mean(pred.num)+0.5*sd(pred.num)) %>%
+  
+  ggplot(aes(y=mean, x=Treatment)) + geom_point(size=2, colour="firebrick")+
+  geom_errorbar(aes(ymin=lcl, ymax=ucl), width=.03)+
+  scale_y_continuous(limits=c(0,3)) +
+  facet_wrap(~Phase, ncol=2) +
+  xlab("") +
+  ylab("Predicted number of LTDU") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+ggsave("LEB_Treatment_effect_byPhase_BACI_withMaris.jpg", width=8, height=11)
