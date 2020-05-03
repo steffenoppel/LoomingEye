963d6662077a1f4bb4fa1c5cf11a1203559fc05e
Steffen <steffen.oppel@gmail.com>
2020-05-03 07:31:32 +0100

    added analysis for eiders and mergansers
    very limited data and models not converging when 'Observer' is included

--- Deterrence_LEB_analysis.R
+++ Deterrence_LEB_analysis.R
@@ -5,7 +5,8 @@
 
 ## originally created 19 April 2020
 ## modified on 28 April 2020 to include Yann's comments
-## removed counts from Maris as they seem to be completely opposite to anyone else
+## temporarily removed counts from Maris as they seem to be completely opposite to anyone else - but then reinserted
+## updated on 3 May 2020: included sections for other species
 
 ### Load libraries
 library(ggplot2)
@@ -15,6 +16,7 @@ library(stringr)
 library(lubridate)
 library(readxl)
 library(randomForest)
+library(glmmTMB)
 filter<-dplyr::filter
 select<-dplyr::select
 
@@ -49,7 +51,7 @@ surveys<-surveys[,c(2,3,8:19)] %>%
 
 dim(surveys)
 
-counts<-counts[,c(1:96)] %>% select(-c(1,3,5,which(grepl("SUM",names(counts))),which(grepl("Additional",names(counts))))) %>%
+counts<-counts[,c(1:96)] %>% select(-c(1,3,5,which(grepl("SUM",names(counts))),which(grepl("Additional",names(counts))))) %>%  ## THIS ELIMINATES 40 birds across all phases that are lumped in 'additional 1 species'
   rename(Time=`Count Time:`, CountSeq=`Count Number:`) %>%
   gather(key=spec,value=Number,-GlobalID,-Phase,-Time,-CountSeq) %>%
   separate(spec,into=c("Treatment","Species"),sep="_") %>%
@@ -368,7 +370,6 @@ ggsave("LEB_Treatment_effect_byPhase.jpg", width=8, height=11)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ##### 5. CONDUCT PARAMETRIC BACI ANALYSIS ######
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-library(glmmTMB)
 
 ### MANIPULATE DATA TO INCLUDE BEFORE-AFTER
 head(data)
@@ -436,3 +437,184 @@ effsize<-plotdat %>%
   spread(key=Treatment, value=mean) %>%
   mutate(reduction=((Control-Treatment)/Control)*100)
 fwrite(effsize,"LEB_BACI_effect_size_perPhase.csv")
+
+
+
+
+
+
+
+
+
+
+#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
+#####     FORMAL ANALYSIS FOR EIDERS AND SCOTERS      ~~~~~~~~~~~########
+#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
+unique(counts$Species)
+
+### SELECT ONLY THE RECORDS RELEVANT TO THE TEST
+data<- counts %>% filter(!is.na(Number)) %>% 
+  filter(Species %in% c("Unknown Eiders","Steller's Eider","Common Eider")) %>%   ##"Unknown Scoters","Velvet Scoter","Common Scoter",
+  mutate(Observer=ifelse(Observer=="Ainar,Mati","Mati,Ainar",Observer)) %>%
+  mutate(Observer=ifelse(Observer=="Ainar,Andrus,Rein,Veljo,Mati,Andres,OtherObservers","OtherObservers",Observer)) %>%
+  mutate(Observer=ifelse(Observer=="Veljo,OtherObservers","Veljo",Observer)) %>%
+  group_by(Phase,Treatment,CountSeq,PeriodCode,Observer,weather,wind_dir,Temp,wind_speed,cloud,sea,vis,Start) %>%
+  summarise(N=sum(Number)) %>%
+  mutate(day=yday(Start)) %>%
+  mutate(hour=hour(Start)) %>%
+  ungroup() %>%
+  mutate_if(is.character, as.factor)
+
+hist(data$N)
+dim(data)
+table(data$Observer)
+
+
+
+##### CONDUCT PARAMETRIC BACI ANALYSIS ######
+
+BACIdata <- data %>% mutate(Before=ifelse(Phase=="PrePhase1","before","after")) %>%
+  mutate(PhaseNum=ifelse(Phase %in% c("Phase2","PostPhase2"),2,1)) %>%
+  mutate(Memory=ifelse(Phase %in% c("PostPhase1","PostPhase2"),1,0))
+
+## fit models - tried all options to include more interactions and variables, but convergence problems if we add more
+m1<-glmmTMB(N~Before+Treatment+Before*Treatment+Memory*Treatment +day+hour +(1|PhaseNum), data=BACIdata, ziformula=~1,family=nbinom2)
+m0<-glmmTMB(N~Before+Treatment+Memory*Treatment +day+hour +(1|PhaseNum), data=BACIdata, ziformula=~1,family=nbinom2)
+
+## assess significance of interaction effect
+anova(m0,m1)
+m1sum<-summary(m1)
+
+
+### PLOT predicted OUTPUT ###
+
+plotdat<-bind_rows(replicate(10,data, simplify=F)) %>%
+  mutate(Treatment=rep(rep(unique(data$Treatment), each=dim(data)[1]),5)) %>%
+  mutate(Phase=rep(rep(unique(data$Phase), each=dim(data)[1]), each=2)) %>%
+  mutate(Before=ifelse(Phase=="PrePhase1","before","after")) %>%
+  mutate(PhaseNum=ifelse(Phase %in% c("Phase2","PostPhase2"),2,1)) %>%
+  mutate(Memory=ifelse(Phase %in% c("PostPhase1","PostPhase2"),1,0)) #%>%
+
+
+#### CALCULATE EFFECT SIZE ######
+effsize<-plotdat %>%
+  mutate(pred.num=predict(m1, newdat=plotdat)) %>%
+  group_by(Phase,Treatment) %>%
+  summarise(mean=mean(pred.num)) %>%
+  spread(key=Treatment, value=mean) %>%
+  mutate(reduction=((Control-Treatment)/Control)*100)
+fwrite(effsize,"LEB_BACI_effect_size_perPhase_eiders.csv")
+
+plotdat %>%
+  mutate(pred.num=predict(m1, newdat=plotdat)) %>%
+  group_by(Phase,Treatment) %>%
+  summarise(mean=mean(pred.num), lcl=mean(pred.num)-0.5*sd(pred.num),ucl=mean(pred.num)+0.5*sd(pred.num)) %>%
+  
+  ggplot(aes(y=mean, x=Treatment)) + geom_point(size=2, colour="firebrick")+
+  geom_errorbar(aes(ymin=lcl, ymax=ucl), width=.03)+
+  #scale_y_continuous(limits=c(0,2.5)) +
+  facet_wrap(~Phase, ncol=2) +
+  xlab("") +
+  ylab("Predicted number of eiders and scoters") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+ggsave("LEB_Treatment_effect_byPhase_BACI_eiders.jpg", width=8, height=11)
+
+
+
+
+
+#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
+#####     FORMAL ANALYSIS FOR MERGANSERS      ~~~~~~~~~~~########
+#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~########
+unique(counts$Species)
+
+### SELECT ONLY THE RECORDS RELEVANT TO THE TEST
+data<- counts %>% filter(!is.na(Number)) %>% 
+  filter(Species %in% c("Uknown Mergansers","Red-breasted Merganser","Common Merganser","Smew")) %>%   ##"Unknown Scoters","Velvet Scoter","Common Scoter",
+  mutate(Observer=ifelse(Observer=="Ainar,Mati","Mati,Ainar",Observer)) %>%
+  mutate(Observer=ifelse(Observer=="Ainar,Andrus,Rein,Veljo,Mati,Andres,OtherObservers","OtherObservers",Observer)) %>%
+  mutate(Observer=ifelse(Observer=="Veljo,OtherObservers","Veljo",Observer)) %>%
+  group_by(Phase,Treatment,CountSeq,PeriodCode,Observer,weather,wind_dir,Temp,wind_speed,cloud,sea,vis,Start) %>%
+  summarise(N=sum(Number)) %>%
+  mutate(day=yday(Start)) %>%
+  mutate(hour=hour(Start)) %>%
+  ungroup() %>%
+  mutate_if(is.character, as.factor)
+
+hist(data$N)
+dim(data)
+table(data$Observer)
+
+
+
+##### CONDUCT PARAMETRIC BACI ANALYSIS ######
+
+BACIdata <- data %>% mutate(Before=ifelse(Phase=="PrePhase1","before","after")) %>%
+  mutate(PhaseNum=ifelse(Phase %in% c("Phase2","PostPhase2"),2,1)) %>%
+  mutate(Memory=ifelse(Phase %in% c("PostPhase1","PostPhase2"),1,0))
+
+## fit models - tried all options to include more interactions and variables, but convergence problems if we add more
+m1<-glmmTMB(N~Before+Treatment+Before*Treatment+Memory*Treatment +day+hour +(1|PhaseNum), data=BACIdata, ziformula=~1,family=nbinom2)
+m0<-glmmTMB(N~Before+Treatment+Memory*Treatment +day+hour +(1|PhaseNum), data=BACIdata, ziformula=~1,family=nbinom2)
+
+## assess significance of interaction effect
+anova(m0,m1)
+m1sum<-summary(m1)
+
+
+### PLOT predicted OUTPUT ###
+
+plotdat<-bind_rows(replicate(10,data, simplify=F)) %>%
+  mutate(Treatment=rep(rep(unique(data$Treatment), each=dim(data)[1]),5)) %>%
+  mutate(Phase=rep(rep(unique(data$Phase), each=dim(data)[1]), each=2)) %>%
+  mutate(Before=ifelse(Phase=="PrePhase1","before","after")) %>%
+  mutate(PhaseNum=ifelse(Phase %in% c("Phase2","PostPhase2"),2,1)) %>%
+  mutate(Memory=ifelse(Phase %in% c("PostPhase1","PostPhase2"),1,0)) #%>%
+
+
+#### CALCULATE EFFECT SIZE ######
+effsize<-plotdat %>%
+  mutate(pred.num=predict(m1, newdat=plotdat)) %>%
+  group_by(Phase,Treatment) %>%
+  summarise(mean=mean(pred.num)) %>%
+  spread(key=Treatment, value=mean) %>%
+  mutate(reduction=((Control-Treatment)/Control)*100)
+#fwrite(effsize,"LEB_BACI_effect_size_perPhase_mergansers.csv")
+
+plotdat %>%
+  mutate(pred.num=predict(m1, newdat=plotdat)) %>%
+  group_by(Phase,Treatment) %>%
+  summarise(mean=mean(pred.num), lcl=mean(pred.num)-0.5*sd(pred.num),ucl=mean(pred.num)+0.5*sd(pred.num)) %>%
+  
+  ggplot(aes(y=mean, x=Treatment)) + geom_point(size=2, colour="firebrick")+
+  geom_errorbar(aes(ymin=lcl, ymax=ucl), width=.03)+
+  #scale_y_continuous(limits=c(0,2.5)) +
+  facet_wrap(~Phase, ncol=2) +
+  xlab("") +
+  ylab("Predicted number of mergansers") +
+  theme(panel.background=element_rect(fill="white", colour="black"), 
+        axis.text=element_text(size=16, color="black"), 
+        axis.title=element_text(size=18), 
+        strip.text=element_text(size=18, color="black"),
+        legend.text=element_text(size=14, color="black"),
+        legend.title=element_text(size=18, color="black"),
+        legend.key=element_blank(),
+        strip.background=element_rect(fill="white", colour="black"), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank())
+
+#ggsave("LEB_Treatment_effect_byPhase_BACI_mergansers.jpg", width=8, height=11)
+
+
